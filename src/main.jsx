8b7302962bed90f2e2e971360e4417f011032b6c
Adrianxx1 <triaxis12q@gmail.com>
2025-07-03 23:37:30 -0600

    aplicando themeDark

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
+import {mode} from '@chakra-ui/theme-tools'
+import { color } from "framer-motion";
+
+const styles = {
+  global: (props) => ({
+    body:{
+      bg:mode("gray.100","#000")(props),
+      color:mode("gray.800", "whiteAlpha.900")(props)
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: 'false',
+} 
+const theme = extendTheme({ config,styles })
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
