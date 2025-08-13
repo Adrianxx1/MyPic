33df2a81b1b241d7fd879eff510c8fb95cbed73b
Adrianxx1 <triaxis12q@gmail.com>
2025-08-13 10:56:43 -0600

    suggested user

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,34 +1,41 @@
-import { Box, Flex, Text, VStack } from "@chakra-ui/react"
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
-import { Link } from "react-router-dom";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
 const SuggestedUsers = () => {
-    return (
-    <VStack py={8} px={6} gap={4}>
-        <SuggestedHeader />
+	const { isLoading, suggestedUsers } = useGetSuggestedUsers();
 
-        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-            <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-                Suggested for you
-            </Text>
-            <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
-                See All
-            </Text>
-        </Flex>
+	// optional: render loading skeleton
+	if (isLoading) return null;
 
-        <SuggestedUser name='Dan Abrahmov' followers={1392} avatar='https://bit.ly/dan-abrahmov' />
-        <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence' />
-        <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-beast' />
+	return (
+		<VStack py={8} px={6} gap={4}>
+			<SuggestedHeader />
 
-        <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-            ©2025 Build By{" "}
-            <Link href='' target='_blank' color='blue.500' fontSize={14}>
-                MyPicTeam
-            </Link>
-        </Box>
-    </VStack>
-    );
+			{suggestedUsers.length !== 0 && (
+				<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+					<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+						Suggested for you
+					</Text>
+					<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+						See All
+					</Text>
+				</Flex>
+			)}
+
+			{suggestedUsers.map((user) => (
+				<SuggestedUser user={user} key={user.id} />
+			))}
+
+			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+				© 2023 Built By{" "}
+				<Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
+					As a Programmer
+				</Link>
+			</Box>
+		</VStack>
+	);
 };
 
-export default SuggestedUsers
+export default SuggestedUsers;
