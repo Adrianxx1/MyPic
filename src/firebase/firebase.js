5bbd1f135ae7285f111b9388f1b35752d5353e66
Luis Adrian Amador Martínez <triaxis12q@gmail.com>
2025-11-23 10:16:05 -0600

    Delete post fixed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAJa7h_K467bO2GOfZkyB1DU8wjVJa_gfc",
   authDomain: "mypic-social-3710b.firebaseapp.com",
   projectId: "mypic-social-3710b",
-  storageBucket: "mypic-social-3710b.appspot.com", // 🔧 corregido
+  storageBucket: "mypic-social-3710b.firebasestorage.app", // ✅ CORREGIDO
   messagingSenderId: "23107529173",
   appId: "1:23107529173:web:942eec6f37d30a7e41f55c",
   measurementId: "G-7KKZSGCD4C",
